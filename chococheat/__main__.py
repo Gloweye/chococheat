98d36908dec14261cf6cd9518d79f59050bc57be
Jacco van Dorp <jaccovandorp@acknowledge.nl>
2022-12-17 00:43:08 +0100

    Item replacer made.

--- chococheat/__main__.py
+++ chococheat/__main__.py
@@ -1,5 +1,5 @@
 """Wizard giving access to the scripts."""
-import enum
+from enum import Enum
 import sys
 from argparse import ArgumentParser
 from inspect import signature
@@ -7,7 +7,8 @@ from pathlib import Path
 from time import sleep, time
 from logging import getLogger, basicConfig, INFO
 
-from chococheat.world_info import is_chicobo_away, CHEATSAVE, CHOCOSAVE
+from chococheat.world_info import (is_chicobo_away, CHEATSAVE, CHOCOSAVE, OFFSET_ITEMS_A, OFFSET_ITEMS_B,
+                                   OFFSET_ITEMS_C, OFFSET_ITEMS_D, RANK_OFFSET, CURRENT_HP_OFFSET, MAX_HP_OFFSET)
 
 
 logger = getLogger(__name__)
@@ -29,6 +30,12 @@ def cli_endpoint(func=None, /, **kwargs):
         return deco
 
 
+class SaveType(str, Enum):
+    primary = 'primary'
+    cheat = 'cheat'
+    backup = 'backup'
+
+
 class CLITool:
     """Manages CLI endpoints. Quick and Dirty kind."""
 
@@ -47,19 +54,88 @@ class CLITool:
             while True:
                 sleep(0.2)
                 if current_age != CHOCOSAVE.stat().st_mtime_ns and is_chicobo_away(CHOCOSAVE):
-                    logger.info(f'{int(time())} Change detected, altering chocobo save...')
+                    logger.info(f'Change detected, replacing chocobo save...')
                     copy_from_cheat_file(CHOCOSAVE, CHEATSAVE)
                     current_age = CHOCOSAVE.stat().st_mtime_ns
         except KeyboardInterrupt:
             logger.info('ChocoCheat has exited.')
 
+    @cli_endpoint(
+        main='Which save to print the status of; defaults to the cheatsave.'
+    )
+    def status(self, *, main: SaveType = SaveType.cheat):
+        """
+        Print the status of a chocoworld save.
+        """
+        subject = {
+            SaveType.cheat: CHEATSAVE,
+            SaveType.primary: CHOCOSAVE,
+            SaveType.backup: CHOCOSAVE,  # @TODO: Actual backup management.
+        }[main]
+        if not subject.exists():
+            logger.info(f'Could not find the {main} file.')
+            return
+
+        filedata = subject.read_bytes()
+
+        if is_chicobo_away(subject):
+            logger.info('Chicobo is currently exploring. That means we can cheat.')
+        else:
+            logger.info('Chicobo is currently home. That means we can\'t meaningfully edit the save file.')
+
+        logger.info(f'Your current rank is {filedata[RANK_OFFSET].to_bytes(1, "big").hex()}. (Smaller is better)')
+        min_hp = filedata[CURRENT_HP_OFFSET].to_bytes(1, 'big').hex()
+        max_hp = filedata[MAX_HP_OFFSET].to_bytes(1, 'big').hex()
+        logger.info(f'Your Current HP is {min_hp}/{max_hp}')
+
+        if filedata[OFFSET_ITEMS_A].to_bytes(1, 'big').hex():
+            # Consider it valid item data
+            for key, value in {
+                'A': filedata[OFFSET_ITEMS_A].to_bytes(1, 'big').hex(),
+                'B': filedata[OFFSET_ITEMS_B].to_bytes(1, 'big').hex(),
+                'C': filedata[OFFSET_ITEMS_C].to_bytes(1, 'big').hex(),
+                'D': filedata[OFFSET_ITEMS_D].to_bytes(1, 'big').hex(),
+            }.items():
+                if value:
+                    logger.info(f'The save has {value} {key}-class items.')
+
+    @cli_endpoint(
+        item_a='Number of items of category A to give, range 0-99 inclusive.',
+        item_b='Number of items of category B to give, range 0-99 inclusive.',
+        item_c='Number of items of category C to give, range 0-99 inclusive.',
+        item_d='Number of items of category D to give, range 0-99 inclusive.',
+    )
+    def items(self, *, item_a: int = None, item_b: int = None, item_c: int = None, item_d: int = None):
+        """Changes the item numbers set in the cheatsave."""
+        data = bytearray(CHEATSAVE.read_bytes())
+        if not data[OFFSET_ITEMS_A].to_bytes(1, 'big').hex():
+            logger.critical('Cheat Save is not in a state that we know how to modify number of items.')
+            logger.info('Please play the chocobo game until you have at least 2 kinds of items.')
+            return 1
+        table = {
+            'A': (item_a, OFFSET_ITEMS_A),
+            'B': (item_b, OFFSET_ITEMS_B),
+            'C': (item_c, OFFSET_ITEMS_C),
+            'D': (item_d, OFFSET_ITEMS_D),
+        }
+        for symbol, (item, offset) in table.items():
+            if item is not None:
+                if 0 <= item < 100:
+                    data[offset] = int.from_bytes(bytes.fromhex(f'{item:0<2}'), 'big')
+                    logger.info(f'There are now {item} {symbol}-class items.')
+                else:
+                    logger.info(f'Number of items provided for {symbol} out of range 0-99 inclusive.')
+
+        CHEATSAVE.write_bytes(data)
+
 
 if __name__ == '__main__':
-    basicConfig(format='{levelname: <8}{name: <20}:{lineno: <4}:{msg}', style='{', stream=sys.stdout, level=INFO)
+    fmt = '{levelname: <8}{name: <20}:{lineno: <4}:{msg}' if '--verbose' in sys.argv else '{msg}'
+    basicConfig(format=fmt, style='{', stream=sys.stdout, level=INFO)
     parser = ArgumentParser(
         'ChocoCheat',
-        'python3 -m chococheat',
-        'Cheat program to make FF8 really easy from the Chocobo World'
+        'python -m chococheat',
+        'Cheat program to make FF8 really easy by messing with the Chocobo World save file.'
     )
     subs = parser.add_subparsers(title='command', dest='command')
     holder = CLITool()
@@ -72,24 +148,36 @@ if __name__ == '__main__':
                     continue
 
                 kwargs = {}
+                prefix = ''
                 if param.default is not param.empty:
                     kwargs['default'] = param.default
-                if issubclass(param.annotation, str) and issubclass(param.annotation, enum.Enum):
-                    kwargs['choices'] = param.annotation.__members__.values()
+                    kwargs['required'] = False
+                    if param.kind is param.KEYWORD_ONLY:
+                        prefix = '--'
+                elif param.annotation is not bool and param.kind is param.KEYWORD_ONLY:
+                    kwargs['required'] = True
+                    prefix = '--'
+
+                if issubclass(param.annotation, str) and issubclass(param.annotation, Enum):
+                    kwargs['choices'] = tuple(mem for mem in param.annotation.__members__)
                 if param.annotation is bool:
                     kwargs['action'] = 'store_false' if param.default is True else 'store_true'
                 else:
                     kwargs['action'] = 'store'
                     kwargs['type'] = param.annotation
-                prefix = '--' if param.kind is param.KEYWORD_ONLY or param.annotation is bool else ''
-                if prefix and param.annotation is not bool:
-                    kwargs['required'] = param.default == param.empty
                 if name in param_info:
                     kwargs['help'] = param_info[name]
+                if '_' in name:
+                    kwargs['dest'] = name
+                    name = name.replace('_', '-')
+
                 command_parser.add_argument(prefix + name, **kwargs)
 
     args = parser.parse_args()
     command = vars(args).pop('command')
+    if command is None:
+        parser.print_help()
+        raise SystemExit(0)
     result = getattr(holder, command)(**vars(args))
     if result and isinstance(result, int) and 0 < result <= 127:  # I remember something about higher being invalid.
         raise SystemExit(result)
--- chococheat/world_info.py
+++ chococheat/world_info.py
@@ -11,11 +11,34 @@ OFFSET_ITEMS_B = 0x1d
 OFFSET_ITEMS_C = 0x1e
 OFFSET_ITEMS_D = 0x1f
 AWAY_OFFSET = 0x15  # To determine whether the Chicobo is "away", and therefore editable.
+LEVEL_OFFSET = 0x08
+CURRENT_HP_OFFSET = 0x09
+MAX_HP_OFFSET = 0x0a
+RANK_OFFSET = 0x0e
+LOWER_ID_OFFSET = 0x13
+HIGHER_ID_OFFSET = 0x14
 
 # Actually only know this about Windows for certain.
 GAME_SAVES_DIR = Path.home() / 'Documents' / 'Square Enix' / 'FINAL FANTASY VIII Steam' / f'user_{config:global.user_id!i}'
 CHOCOSAVE = GAME_SAVES_DIR / 'chocorpg.ff8'
+BACKUPSAVE = GAME_SAVES_DIR / 'chocorpg.ff8.bak'
 CHEATSAVE = GAME_SAVES_DIR / 'chocorpg.ff8.cheat'
+
+
+# Fully valid file. With max items, best weapon, best rank, and no powerups.
+# Unlike pocket-station, it seems cross-save compatible, though cross-player hasn't been tested.
+DEMO_FILE = b'\xb3\x00\x00\x00\xff\xff\x08\x83\x02\x16\x16\x99\x99\xf7\x00\x05' \
+            b'\x0b\xeb\xf0b\x05\x01\x02\xff\x14/\x08\x04\x99\x99\x99\x99' \
+            b'\xff\x02\x08\x01\x05\x11\x06\x01\x0b\xff\x01\t\x00\x03\x10\x03' \
+            b'\x03\x01\xdf\xb4#\x8b\x00\xc8\x04\x00\x06\x01\xbb\x00\x00 ' \
+            b'\x00\x05""\xf4\xf0\x02\x87\x02\x03\xd8\xdc\xffB\x0fT' \
+            b'\x0f\xe5\xf6$\xffsA\x005\x00!\x00\x03\xf7\x02\x00' \
+            b'\x03l\x05E\x00]\x00\xef\x0f\x02\x03\x0fl\x05K\x00' \
+            b'`\xdf\x00\x10\x01\x03\x10l\x05\x00\x00\x05\x17\x04\x00\x07' \
+            b'k\x06\xa4\x0f\xb6\x0f\xc8\x0f\xda\x03\x00\xef\x0f\x01\x1f\x13' \
+            b'\x1f%\x1f7\x1fI\x1f[\x1fm\x1f\xc0\x7f\x1f\x91\x1f' \
+            b'\xa3\x1f\xb5\x1f\xc7\x1f\xe8\xf3b\x05\xf7\x99\x99\x06\xe9\xf2' \
+            b'0\x02\x00\x00\x03\xc5\x07'
 
 
 def is_chicobo_away(path: Path):
